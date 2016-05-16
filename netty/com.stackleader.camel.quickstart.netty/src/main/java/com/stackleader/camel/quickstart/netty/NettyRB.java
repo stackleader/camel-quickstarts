2bb278c864028bfbde6675d692675a781deeaab3
John Eckstein <jeckste@gmail.com>
2016-05-16 18:17:51 -0400

    netty

---
+++ netty/com.stackleader.camel.quickstart.netty/src/main/java/com/stackleader/camel/quickstart/netty/NettyRB.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stackleader.camel.quickstart.netty;
+
+import aQute.bnd.annotation.component.Activate;
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Deactivate;
+import aQute.bnd.annotation.component.Reference;
+import org.apache.camel.CamelContext;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.core.osgi.OsgiDefaultCamelContext;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author jeckstei
+ */
+@Component
+public class NettyRB extends RouteBuilder {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NettyRB.class);
+
+    private CamelContext context;
+
+    @Activate
+    public void activate(BundleContext bundleContext) throws Exception {
+        context = new OsgiDefaultCamelContext(bundleContext);
+        context.addRoutes(this);
+        context.start();
+    }
+
+    @Deactivate
+    public void deactivate() throws Exception {
+        context.stop();
+    }
+
+    @Override
+    public void configure() throws Exception {
+
+        from("netty4:tcp://0.0.0.0:8181?textline=true&sync=true")
+                .log(LoggingLevel.INFO, "before ${body}")
+                .to("bean:" + WordReverseService.class.getCanonicalName() + "?method=reverseWords")
+                .log(LoggingLevel.INFO, "after ${body}");
+    }
+
+    @Reference(target = "(component=netty4)")
+    public void waitForCamelComponentNetty4(org.apache.camel.spi.ComponentResolver cr) {
+
+    }
+}
---
+++ netty/com.stackleader.camel.quickstart.netty/src/main/java/com/stackleader/camel/quickstart/netty/WordReverseService.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stackleader.camel.quickstart.netty;
+
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Reference;
+
+/**
+ *
+ * @author jeckstei
+ */
+@Component(provide = WordReverseService.class)
+public class WordReverseService {
+
+    /**
+     * Copied from
+     * http://codereview.stackexchange.com/questions/37364/reversing-words-in-a-string
+     *
+     * @param sentence
+     * @return
+     */
+    public String reverseWords(String sentence) {
+        StringBuilder sb = new StringBuilder(sentence.length() + 1);
+        String[] words = sentence.split(" ");
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]).append(' ');
+        }
+        sb.setLength(sb.length() - 1);  // Strip trailing space
+        return sb.toString();
+    }
+
+}
